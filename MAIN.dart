56584be62fe9b7c4ac25f4025eac9dfcff31aa28
Sebastian Moreno Castañeda <smorenoc@car.gov.co>
2020-02-21 16:30:35 -0500

    comit encima del primer commit remoto

---
+++ MAIN.dart
@@ -0,0 +1,33 @@
+
+void main() {
+
+  //PARA DECLARAR UNA VARIABLE 
+  VAR H;
+  // PARA DECLARAR VARIABLE DINAMICA
+  Dinamyc cambiante;
+  // PARA DECLARAR VARIBLE NUMERICA
+ int h=10;
+ // PARA DECLARAR VARIBLE DOUBLE
+ double k=10.5;
+ // PARA DECLARAR STRING
+  String cadena = "semana";
+ //PARA DECLARAR BOOLEAN
+  bool t=true;
+ //PARA MOSTRAR EL RESULTADO
+   print ("estamos en la $cadena"
+ //se agrega otro texto  
+   "se puede agregar otra cadena");
+   //admite comillas sencillas o doble
+    print ('hola');
+    print ("hola 2");  
+   // genera salto de linea '''
+   print ('''estamos en la $cadena
+   salto de linea''')
+   // aregando r al inicio muestra todos los simbolos
+   print (r"sda4d65as4d8as7d/*as7da");
+   // genera salto de carro normal sencillo
+  print ("sda4d65as4d8as7d/*as7da \n");
+  //comportamiento de clase
+  print(cadena.isEmpty);
+
+}
